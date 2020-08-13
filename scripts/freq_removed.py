11bbd5cbfec54a66c09674c7b10e1f50f9954c62
Uros Tesic <uros.tesic@mail.com>
2020-08-13 17:44:24 +0200

    Graph data

--- scripts/freq_removed.py
+++ scripts/freq_removed.py
@@ -6,14 +6,18 @@ import seaborn
 # In the case of ops per second, the numbers are unchanged
 # In the case of time, its inverse is used
 
-TIKTOK_OFF = [1/110.41, 2277.0, 3144799.0, 2435308.0, 1/1002.0, 1/49.01, 41988.0, 43640.0, 3790028.0]
-TIKTOK_PART = [1/110.9, 2269.0, 3125000.0, 2437066.0, 1/996.0, 1/48.62, 35416.0, 37796.0, 3619155.0]
-TIKTOK_FULL = [1/110.41, 2277.2, 3125020.33, 2427298.3, 1/1001.0, 1/48.64, 34905.39, 36493.69, 3597080.0]
+TIKTOK_OFF = [41988.0, 43640.0]
+TIKTOK_PART = [35416.0, 37796.0]
+TIKTOK_FULL = [34905.39, 36493.69]
+TIKTOK_FREQ = [39744.0, 42429.0]
+TIKTOK_NOCALLS = [40807.0, 42755.0]
 
 
 tiktok_off_percent = [100.0] * len(TIKTOK_OFF)
 tiktok_part_percent = []
 tiktok_on_percent = []
+tiktok_freq_removed_percent = []
+tiktok_nocalls_percent = []
 
 for i in range(len(TIKTOK_FULL)):
     tiktok_on_percent.append(100.0 * TIKTOK_FULL[i]/ TIKTOK_OFF[i])
@@ -21,12 +25,20 @@ for i in range(len(TIKTOK_FULL)):
 for i in range(len(TIKTOK_PART)):
     tiktok_part_percent.append(100.0 * TIKTOK_PART[i]/ TIKTOK_OFF[i])
 
+for i in range(len(TIKTOK_FREQ)):
+    tiktok_freq_removed_percent.append(100.0 * TIKTOK_FREQ[i]/ TIKTOK_OFF[i])
+
+for i in range(len(TIKTOK_NOCALLS)):
+    tiktok_nocalls_percent.append(100.0 * TIKTOK_NOCALLS[i]/ TIKTOK_OFF[i])
+
 
 df = pandas.DataFrame({
-    'Benchmark': ['Timed Linux \nKernel Compilation \nv5.4', 'OpenSSL v1.1.1 \nRSA 4096-bit', 'Redis v5.0.5 \nGET', 'Redis v5.0.5 \nSET', 'PyBench \nv2018-02-16', 'Git', 'Apache Benchmark \nv2.4.29', 'NGINX Benchmark \nv1.9.9', 'IPC TCP 128b'],
-    'TikTok OFF': tiktok_off_percent,
-    'TikTok PART': tiktok_part_percent,
-    'TikTok ON': tiktok_on_percent
+    'Benchmark': ['Apache Benchmark \nv2.4.29', 'NGINX Benchmark \nv1.9.9'],
+    'TikTok Off': tiktok_off_percent,
+    'TikTok On - No Calls Protected': tiktok_nocalls_percent,
+    'TikTok On - No Writes + Frequent Calls Removed': tiktok_freq_removed_percent,
+    'TikTok On - No Writes': tiktok_part_percent,
+    'TikTok On': tiktok_on_percent
 })
 fig, ax1 = pyplot.subplots(figsize=(27, 10))
 tidy = df.melt(id_vars='Benchmark').rename(columns=str.title)
